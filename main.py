aa991bb24e8f3498e15a61aa05d99d587d725a0b
zolcsika71 <bushdoctor2008@gmail.com>
2024-07-06 04:09:32 +0200

    The provided diff does not match the commit message. The diff is about importing a class from a Python module in "main.py" file, not about adding a new XML configuration file. Please provide the appropriate diff.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from database import DatabaseManager
+# imports
+
+from database.create_db import DatabaseManager
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
